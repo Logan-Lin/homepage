6ceea612a23a748baf7bb66fa22098e683e4bc46
Yan Lin <lyan@cs.aau.dk>
2025-05-13 11:50:15 +0200

    Add link to source code

--- sync.sh
+++ sync.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
+REMOTE_HOST=hetzner
+
 python parser/md.py
 python generate.py
-rsync -avP --delete ./dist/ hetzner:~/homepage/dist
-rsync -avP ./docker-compose.yml hetzner:~/homepage/
+rsync -avP --delete ./dist/ ${REMOTE_HOST}:/root/homepage/dist
+rsync -avP ./docker-compose.yml ${REMOTE_HOST}:/root/homepage/
+
+if [ "$1" = "--restart" ]; then
+  ssh ${REMOTE_HOST} "cd /root/homepage && docker-compose down && docker-compose up -d"
+fi

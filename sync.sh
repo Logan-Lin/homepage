f747f57145376159359616b8ccc5d8d8622309ea
Yan Lin <lyan@cs.aau.dk>
2025-05-14 11:02:11 +0200

    Improve responsive nav bar

--- sync.sh
+++ sync.sh
@@ -8,5 +8,5 @@ rsync -avP --delete ./dist/ ${REMOTE_HOST}:/root/homepage/dist
 rsync -avP ./docker-compose.yml ${REMOTE_HOST}:/root/homepage/
 
 if [ "$1" = "--restart" ]; then
-  ssh ${REMOTE_HOST} "cd /root/homepage && docker-compose down && docker-compose up -d"
+  ssh ${REMOTE_HOST} "cd /root/homepage && docker compose down && docker compose up -d"
 fi

ac07362535f759a308df811e3498bb75728751fd
Yan Lin <lyan@cs.aau.dk>
2025-05-15 22:46:36 +0200

    Add nix shell config

---
+++ shell.nix
@@ -0,0 +1,17 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  packages = with pkgs; [
+    python312
+    python312Packages.pip
+    python312Packages.virtualenv
+  ];
+
+  shellHook = ''
+    if [ ! -d .venv ]; then
+      python -m venv .venv
+    fi
+    source .venv/bin/activate
+    pip install -r requirements.txt
+  '';
+}

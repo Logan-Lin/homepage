51d12d7ab142ea07d7f702adcb5b3789b00ae01b
Yan Lin <linyanwd213@gmail.com>
2025-07-09 14:10:52 +0200

    Enable dynamic ordering of sections

--- generate.py
+++ generate.py
@@ -26,9 +26,22 @@ if __name__ == '__main__':
 
         print(f'Generated {output_path}')
 
+    # Always generate the main index page
     render_template('index.html', 'dist/index.html', data=profile_data, is_home_page=True)
-    render_template('publications.html', 'dist/publications/index.html', data=profile_data, is_home_page=False)
-    render_template('projects.html', 'dist/projects/index.html', data=profile_data, is_home_page=False)
-    render_template('presentations.html', 'dist/presentations/index.html', data=profile_data, is_home_page=False)
-    render_template('teaching.html', 'dist/teaching/index.html', data=profile_data, is_home_page=False)
+    
+    # Generate individual section pages only if they're in the sections list
+    sections = profile_data.get('sections', [])
+    
+    if 'publications' in sections:
+        render_template('publications.html', 'dist/publications/index.html', data=profile_data, is_home_page=False)
+    
+    if 'projects' in sections:
+        render_template('projects.html', 'dist/projects/index.html', data=profile_data, is_home_page=False)
+    
+    if 'presentations' in sections:
+        render_template('presentations.html', 'dist/presentations/index.html', data=profile_data, is_home_page=False)
+    
+    if 'teaching' in sections:
+        render_template('teaching.html', 'dist/teaching/index.html', data=profile_data, is_home_page=False)
+    
     print('Static site generation complete!')
